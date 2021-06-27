09ddf455a82b6e7fd84142a5ce0f7f8419d7278d
lanyangjia <121601585@qq.com>
2021-06-27 16:59:47 +0800

    spring:test push

--- spring-lyj/src/main/java/com/spring/lyj/Test.java
+++ spring-lyj/src/main/java/com/spring/lyj/Test.java
@@ -2,6 +2,9 @@ package com.spring.lyj;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * @author lyj
+ */
 public class Test {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LyjConfig.class);
